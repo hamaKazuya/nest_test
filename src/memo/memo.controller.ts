82712d1b106e3aa18c9104178a53c07660b8fe38
hamaKazuya <haman.kazuya@gmail.com>
2021-02-17 14:18:38 +0900

    一通り

--- src/memo/memo.controller.ts
+++ src/memo/memo.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common'
+import { MemoService } from './memo.service'
 
 @Controller('memo')
-export class MemoController {}
+export class MemoController {
+  constructor(private readonly service: MemoService) {}
+
+  @Get()
+  getMemoList() {
+    return this.service.getMemoList()
+  }
+
+  @Post()
+  addMemo(@Query() query: { name: string; description: string }) {
+    return this.service.addMemo(query.name, query.description)
+  }
+}
--- src/memo/memo.module.ts
+++ src/memo/memo.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { MemoController } from './memo.controller';
-import { MemoService } from './memo.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { MemoController } from './memo.controller'
+import { Memo } from '../entities/memo.entity'
+import { MemoService } from './memo.service'
 
 @Module({
   controllers: [MemoController],
-  providers: [MemoService]
+  imports: [TypeOrmModule.forFeature([Memo])],
+  providers: [MemoService],
 })
 export class MemoModule {}
